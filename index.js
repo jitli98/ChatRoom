0e7b9a06c603a481f2a70de5be5236a0ff729bae
Jit Gan <jitli98@live.com>
2019-09-05 20:37:20 -0700

    Added users functionality.

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
+    // Broadcast message to all users when a new user enters the chat
+    socket.on('new user', (msg) => {
+        socket.username = msg;
+        io.emit('new user', socket.username);
+    })
+
+    // Handles messages in the chatroom
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        io.emit('chat message', msg);   
+    });
+
+    // Broadcast message to all users when someone leaves the chat
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave room', socket.username);
     });
 });
 
